d8cd01ae88853323008f457b1a5c5cd9314555ab
iwan-dyke <iwandyke@hotmail.co.uk>
2026-06-23 14:56:23 +0100

    Start the workbench leader key surface

--- doom/modules/system/keybindings.el
+++ doom/modules/system/keybindings.el
@@ -1,1 +1,6 @@
 ;;; system/keybindings.el -*- lexical-binding: t; -*-
+
+(map! :leader
+      (:prefix-map ("w" . "workbench")
+       :desc "Show profile" "p" #'workbench/show-profile
+       :desc "Show default AI tool" "a" #'workbench/show-default-ai-tool))
