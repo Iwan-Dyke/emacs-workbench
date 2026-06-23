8ebfffc6771ced21d0acc9824d4396341a3f457e
iwan-dyke <iwandyke@hotmail.co.uk>
2026-06-23 14:31:03 +0100

    Carry launcher profile into Emacs config

--- doom/modules/system/core.el
+++ doom/modules/system/core.el
@@ -1,3 +1,15 @@
 ;;; system/core.el -*- lexical-binding: t; -*-
 
 (setq user-full-name "Iwan-Dyke")
+
+(defvar workbench/profile
+  (let ((profile (or (getenv "WORKBENCH_PROFILE") "personal")))
+    (if (member profile '("personal" "work"))
+        profile
+      "personal"))
+  "Active Emacs Workbench profile.")
+
+(defun workbench/show-profile ()
+  "Show the active Emacs Workbench profile."
+  (interactive)
+  (message "Workbench profile: %s" workbench/profile))
