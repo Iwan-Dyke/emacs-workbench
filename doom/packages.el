382288be2a4b603b6f331348cd12c47d03b0558c
Iwan Dyke <iwandyke@hotmail.co.uk>
2026-08-22 22:48:06 +0100

    security: Emacs packages unpinned allow upstream takeover

--- doom/packages.el
+++ doom/packages.el
@@ -1,11 +1,15 @@
 ;;; packages.el -*- no-byte-compile: t; -*-
 
-(package! treemacs-evil)
-(package! org-roam)
-(package! ob-mermaid)
-(package! mermaid-mode)
-(package! lin)
-(package! org-modern)
-(package! pulsar)
-(package! dimmer)
-(package! zone-matrix :recipe (:host github :repo "twitchy-ears/zone-matrix"))
+;; All packages pinned to known-good commits for supply chain security.
+;; To update: change the :pin hash, then run `doom sync'.
+
+(package! treemacs-evil :pin "2ab5a3c89fa0")
+(package! org-roam :pin "903bd4ec56d2")
+(package! ob-mermaid :pin "30c2da02e3d2")
+(package! mermaid-mode :pin "804dbcb1452e")
+(package! lin :pin "c16b7d061396")
+(package! org-modern :pin "8775389d085a")
+(package! pulsar :pin "6d54b123f319")
+(package! dimmer :pin "bbab62f01d45")
+(package! zone-matrix :recipe (:host github :repo "twitchy-ears/zone-matrix")
+  :pin "508e2fa6f1d9")
